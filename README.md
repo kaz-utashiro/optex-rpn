20661799ec0d4c4e15ba26fc1db79f04f5ca1091
Kazumasa Utashiro <kaz@utashiro.com>
2021-11-14 09:12:06 +0900

    minil new App::optex::rpn

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::optex::rpn - It's new $module
+
+# SYNOPSIS
+
+    use App::optex::rpn;
+
+# DESCRIPTION
+
+App::optex::rpn is ...
+
+# LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kazumasa Utashiro <kaz@utashiro.com>
---
+++ lib/App/optex/rpn.pm
@@ -0,0 +1,39 @@
+package App::optex::rpn;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::optex::rpn - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::optex::rpn;
+
+=head1 DESCRIPTION
+
+App::optex::rpn is ...
+
+=head1 LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kazumasa Utashiro E<lt>kaz@utashiro.comE<gt>
+
+=cut
+
